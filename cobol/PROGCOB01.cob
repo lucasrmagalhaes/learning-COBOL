cd56bb80fb1671dbe2bb4ca5f2627427eee3ff67
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2020-11-14 11:56:28 -0300

    primeiro programa

---
+++ cobol/PROGCOB01.cob
@@ -0,0 +1,13 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. PROGCOB01.
+      *****************************************
+      * AREA DE COMENTARIOS - REMARKS
+      * AUTHOR = LUCAS  LRM
+      * DATA   = XX/XX/XXXX
+      * OBJETIVO: MOSTRAR A STRING HELLO ALURA
+      *****************************************
+       ENVIRONMENT DIVISION.
+       DATA DIVISION.
+       PROCEDURE DIVISION.
+          DISPLAY 'HELLO ALURA'.
+          STOP RUN.
