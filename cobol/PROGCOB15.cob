72d60a37e4a91012fe02591dcf96ba9106eac08e
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2020-11-29 15:50:36 -0300

    perform until

--- cobol/PROGCOB15.cob
+++ cobol/PROGCOB15.cob
@@ -1,41 +1,41 @@
        IDENTIFICATION DIVISION.
-       PROGRAM-ID. PROGCOB14.
+       PROGRAM-ID. PROGCOB15.
       **********************************************************
       * AREA DE COMENTARIOS - REMARKS
       * AUTHOR: LUCAS  LRM
       * DATA: 29/11/2020
       * OBJETIVO: RECEBER UM NUMERO E GERAR A TABUADA DE 1 A 10
-      * UTILIZAR O PERFORM - VARYING
+      * UTILIZAR O PERFORM - UNTIL
       **********************************************************
        ENVIRONMENT DIVISION.
        DATA DIVISION.
        WORKING-STORAGE SECTION.
-       77 WRK-NUMERO	PIC 9(02)	VALUE ZEROS.
-       77 WRK-CONTADOR	PIC 9(02)	VALUE 1.
-       77 WRK-RESUL	PIC 9(02)	VALUE ZEROS.
+       77 WRK-VENDAS	PIC 9(06)V99	VALUE ZEROS.
+       77 WRK-QT	PIC 9(03)	VALUE 0.
+       77 WRK-ACUM	PIC 9(08)V99	VALUE ZEROS.
        
        PROCEDURE DIVISION.
        0001-PRINCIPAL.
        		PERFORM 0100-INICIALIZAR.
-       		IF WRK-NUMERO > 0 
-        		PERFORM 0200-PROCESSAR
+       		IF WRK-VENDAS > 0 
+        		PERFORM 0200-PROCESSAR UNTIL WRK-VENDAS = 0
         	END-IF.
-        	PERFORM 0300-FINALIZAR.
+        	PERFORM 0300-FINALIZAR
         	
         	STOP RUN.
        
        0100-INICIALIZAR.
-       	       DISPLAY 'DIGITE UM NUMERO PARA REALIZAR A TABUADA: '. 
-       	       ACCEPT WRK-NUMERO.
+       	       DISPLAY 'QUANTIDADE DE VENDAS NO DIA: '. 
+       	       ACCEPT WRK-VENDAS.
        
        0200-PROCESSAR.
-      ******************** FROM = COMECA | BY = INCREMENTO | UNTIL = ATE QUANDO
-               PERFORM VARYING WRK-CONTADOR FROM 1 BY 1
-               		UNTIL WRK-CONTADOR > 10
-     		COMPUTE WRK-RESUL = WRK-NUMERO * WRK-CONTADOR
-               	DISPLAY WRK-NUMERO ' x ' WRK-CONTADOR
-               END-PERFORM.
+               ADD 1 TO WRK-QT.
+               ADD WRK-VENDAS TO WRK-ACUM.
+               PERFORM 0100-INICIALIZAR.
+               
        
        0300-FINALIZAR.
        		DISPLAY '=========================================='
+       		DISPLAY 'TOTAL: ' WRK-ACUM.
+       		DISPLAY 'QUANTIDADE DE REGISTROS: ' WRK-QT.
        		DISPLAY 'FINAL DE PROCESSAMENTO'.
