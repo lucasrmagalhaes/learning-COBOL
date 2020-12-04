22384adeda8df2c0cd15d5e66c52d8a8b13abdf8
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2020-12-04 12:50:27 -0300

    validando a escolha do menu

--- cobol/CLIENTES.cbl
+++ cobol/CLIENTES.cbl
@@ -7,26 +7,22 @@
        ENVIRONMENT DIVISION.
        DATA DIVISION.
        WORKING-STORAGE SECTION.
-       77 WRK-OPCAO PIC X(1).
-       77 WRK-TITULO PIC X(20).
+       77 WRK-OPCAO    PIC X(1).
+       77 WRK-MODULO   PIC X(25).
+       77 WRK-TECLA    PIC X(1).
+
        SCREEN SECTION.
        01 TELA.
            05 LIMPA-TELA.
-      ************************** LIMPAR A TELA
                10 BLANK SCREEN.
-      ************************** POSICIONA NA LINHA 1 E COLUNA 1
-      ************************** END OF LINE (EOL)
                10 LINE 01 COLUMN 01 PIC X(20) ERASE EOL
-      ************************** PASSANDO A COR POR NUMERO
-      ************************** CORES:
-      ************************** 1 - AZUL
-      ************************** 2 - VERDE
-      ************************** 3 - AZUL CLARO
-      ************************** 4 - VERMELHO
-      ************************** 5 - ROXO
-      ************************** 6 - DOURADO
-      ************************** 7 - CINZA
-                   BACKGROUND-COLOR 2 FROM WRK-TITULO.
+                   BACKGROUND-COLOR 3.
+               10 LINE 01 COLUMN 25 PIC X(20)
+      ********************* FOREGROUND-COLOR DEFINE A COR DA FONTE
+                  BACKGROUND-COLOR 3 FOREGROUND-COLOR 0
+                  FROM 'SISTEMA DE CLIENTES'.
+               10 LINE 02 COLUMN 01 PIC X(25) ERASE EOL
+                   BACKGROUND-COLOR 1 FROM WRK-MODULO.
 
        01 MENU.
            05 LINE 07 COLUMN 15 VALUE '1 - INCLUIR'.
@@ -39,7 +35,38 @@
            05 LINE 14 COLUMN 23 USING WRK-OPCAO.
 
        PROCEDURE DIVISION.
-           MOVE '- MENU - ' TO WRK-TITULO.
+       0001-PRINCIPAL SECTION.
+           PERFORM 1000-INICIAR.
+           PERFORM 2000-PROCESSAR.
+           PERFORM 3000-FINALIZAR.
+           STOP RUN.
+
+       1000-INICIAR.
            DISPLAY TELA.
            ACCEPT MENU.
-           STOP RUN.
+
+       2000-PROCESSAR.
+           EVALUATE WRK-OPCAO
+               WHEN 1
+                   PERFORM 5000-INCLUIR
+               WHEN 2
+                   CONTINUE
+               WHEN 3
+                   CONTINUE
+               WHEN 4
+                   CONTINUE
+               WHEN 5
+                   CONTINUE
+               WHEN OTHER
+                   IF WRK-OPCAO NOT EQUAL 'X'
+                       DISPLAY 'ENTRE COM A OPCAO CORRETA'
+                   END-IF
+           END-EVALUATE.
+
+       3000-FINALIZAR.
+           CONTINUE.
+
+       5000-INCLUIR.
+           MOVE 'MODULO - INCLUSAO ' TO WRK-MODULO.
+           DISPLAY TELA.
+           ACCEPT WRK-TECLA AT 1620.
