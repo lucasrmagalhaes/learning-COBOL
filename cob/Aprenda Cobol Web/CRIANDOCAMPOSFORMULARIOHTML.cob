cd428a4ccbbaa64ca542a95bb3a2d10cf7702d40
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-01-06 11:07:11 -0300

    criando formulario html

---
+++ cob/Aprenda Cobol Web/CRIANDOCAMPOSFORMULARIOHTML.cob
@@ -0,0 +1,44 @@
+           EXEC HTML
+               <TABLE width=100%>
+                   <TR>
+                      <TD class=titulocampo>
+                           <CENTER>Aula 2 </CENTER>
+                      <TR>
+                      <TD>
+                           <TABLE rules=none 
+                                  width=100% 
+                                  border=1
+                                  cellspacing=1 
+                                  cellpadding=0
+                                  bordercolor=White 
+                                  bordercolordark=White 
+                                  bordercolorlight=DimGray 
+                                  class=WindoScreen>
+                               <TR>
+                                   <TD colspan=4>&nbsp;</TD>
+                               <TR>
+                                   <TD width=20% class=titulocampo>
+                                       Código&nbsp;
+                                   </TD>
+                                   <TD colspan=3>
+                                       <INPUT class="campo" 
+                                              name=cdCodigo 
+                                              tabindex=1 
+                                              size=07 
+                                              maxlength=7>
+                                   </TD>
+                               <TR>
+                                   <TD class=titulocampo>
+                                       Nome&nbsp;
+                                   </TD>
+                                   <TD colspan=3>
+                                       <INPUT class="campo" 
+                                              name=cdCodigo 
+                                              tabindex=2 
+                                              size=40 
+                                              maxlength=40>
+                                   </TD>
+                           </TABLE>
+                      </TD>
+               </TABLE>
+           END-EXEC.
